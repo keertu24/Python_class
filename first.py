b7039654bc71731068ebf203cd7a3937c6f7d241
keertu24 <keerthankeertu24@gmail.com>
2022-08-23 21:15:19 +0530

    First class

---
+++ first.py
@@ -0,0 +1,74 @@
+# print("Hello world ")
+# lis=[10,20,30,40,50]
+# dic={'Name':'Keerthan','age':20,'Class':"BCA"}
+# tupl=(10,20,30,40)
+# lis[1]=65
+# tupl[2]=65
+# print(lis[1])
+# print(dic['Class'])
+# print(tupl[2])
+
+
+# lis=[1,2,3,4,5,6]
+# for i in lis:
+#     if i==4:
+#         continue
+#     print(i)
+#     # if i==4:
+#     #     break
+
+
+# lis =['Keerthan','Thanu','Harshita','Anisha','Chaitra']
+# for i in range(len(lis)):
+#     print(lis[i])
+
+
+# tuple1=(1,2,3,4,5,6,7,8,9,10,11,12)
+# list1=[]
+# for i in tuple1:
+#     if i%2==0:
+#         list1.append(i)       
+# print(list1)
+
+
+# list2=[12,56,32,11,34]
+# list2.sort()
+# print(list2)
+
+
+# dic={'Name':'Keerthan','age':20,'Class':"BCA"}
+# if dic:
+#     print("Dict is there")
+# else:
+#     print("Dict is empty ")
+
+
+# def even(num):
+#     if num%2==0:
+#         return True
+# lis=[1,2,3,4,5,6,7,8,9]
+# a=filter(lambda num:num%2==0,lis)
+# for i in a:
+#     print(i)
+
+
+# def keertu ():
+#     pass 
+
+
+# for i in range(10):
+#     if(i==5):
+#         print("5 is found")
+#         break  
+# else:
+#     print('5 is not found')
+
+
+str1="helLo woRld!"
+str2="keeRthan"
+# print(str1[-5:-1])
+str3=str1+" "+str2
+
+print(str3.capitalize())
+print(str3.upper())
+print(str3.lower())
